51a1db5c9690f671fa1653427018cfe6a71eb051
tsunagarume <140996867+tsunagarume@users.noreply.github.com>
2023-08-01 15:28:17 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,8 +4,8 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
-let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
+let userID = 'e936e189-4681-4696-8437-af80cd0d1995';
+let proxyIP = "64.68.192.95" + Math.floor(Math.random() * 255);
 
 let address = '';
 let portWithRandomLog = '';
